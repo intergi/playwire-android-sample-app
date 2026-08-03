512454324f3d8b77bd986fea21f7577f821f4a4e
raouf-playwire <ralmazloum@playwire.com>
2026-08-03 10:14:38 -0400

    chore: update playwire dependency

--- demo-java/build.gradle.kts
+++ demo-java/build.gradle.kts
@@ -52,7 +52,7 @@ repositories {
 }
 
 dependencies {
-    implementation("com.intergi.playwire:playwiresdk_total:13.0.0-alpha.3")
+    implementation("com.intergi.playwire:playwiresdk_total:13.0.0")
 
     implementation("com.google.android.material:material:1.13.0")
 
